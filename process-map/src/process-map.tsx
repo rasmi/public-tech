5dba69eef7afc4370d490166bd085e30b2213896
Rasmi Elasmar <rrelasmar@gmail.com>
2022-12-13 17:16:52 -0500

    Added placeholder nodes and edges.

--- process-map/src/process-map.tsx
+++ process-map/src/process-map.tsx
@@ -1,9 +1,35 @@
 import ReactFlow, { Background, Controls } from 'reactflow';
 import 'reactflow/dist/style.css';
 
+const nodes = [
+    {
+        id: '1',
+        type: 'input',
+        data: { label: 'Hello' },
+        position: {x: 0, y: 0}
+    },
+    {
+        id: '2',
+        type: 'output',
+        data: { label: 'World' },
+        position: {x: 0, y: 100}
+    }
+];
+
+const edges = [
+    {
+        id: '1->2',
+        source: '1',
+        target: '2'
+    }
+];
+
 function ProcessMap() {
   return (
       <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        fitView
         proOptions={{ hideAttribution: true }}
       >
         <Background />
