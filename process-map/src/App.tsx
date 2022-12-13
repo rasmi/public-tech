e4bc595549cdd23d5e7992ae729da64a6e7f60ad
Rasmi Elasmar <rrelasmar@gmail.com>
2022-12-13 15:34:46 -0500

    Create-react-app for process map.

---
+++ process-map/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+        <p>
+          Hello!
+        </p>
+    </div>
+  );
+}
+
+export default App;
