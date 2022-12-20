3b86c660b5412bc8dbe9b99cbdb98dc172563933
Rasmi Elasmar <rrelasmar@gmail.com>
2022-12-20 16:56:44 -0500

    Added draggable nodes, horizontal layout.

--- process-map/src/process-map.tsx
+++ process-map/src/process-map.tsx
@@ -1,40 +1,62 @@
-import ReactFlow, { Background, Controls } from 'reactflow';
-import 'reactflow/dist/style.css';
+import { useCallback } from "react";
+import ReactFlow, {
+  Background,
+  Controls,
+  Node,
+  Edge,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
+  Position,
+} from "reactflow";
+import "reactflow/dist/style.css";
 
-const nodes = [
-    {
-        id: '1',
-        type: 'input',
-        data: { label: 'Hello' },
-        position: {x: 0, y: 0}
-    },
-    {
-        id: '2',
-        type: 'output',
-        data: { label: 'World' },
-        position: {x: 0, y: 100}
-    }
+const initialNodes: Node[] = [
+  {
+    id: "understand_resources",
+    type: "input",
+    sourcePosition: Position.Right,
+    data: { label: "Understand which resources are available to me." },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: "understand_eligibility",
+    targetPosition: Position.Left,
+    data: { label: "Understand which resources I am eligible for." },
+    position: { x: 200, y: 0 },
+  },
 ];
 
-const edges = [
-    {
-        id: '1->2',
-        source: '1',
-        target: '2'
-    }
+const initialEdges: Edge[] = [
+  {
+    id: "understand_resources->understand_eligibility",
+    source: "understand_resources",
+    type: "smoothstep",
+    target: "understand_eligibility",
+  },
 ];
 
 function ProcessMap() {
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node[]>(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge[]>(initialEdges);
+  const onConnect = useCallback(
+    (connection: Connection) => setEdges((es) => addEdge(connection, es)),
+    []
+  );
   return (
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        fitView
-        proOptions={{ hideAttribution: true }}
-      >
-        <Background />
-        <Controls />
-      </ReactFlow>
+    <ReactFlow
+      nodes={nodes}
+      onNodesChange={onNodesChange}
+      edges={edges}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+      proOptions={{ hideAttribution: true }}
+    >
+      <Background />
+      <Controls />
+    </ReactFlow>
   );
 }
 
