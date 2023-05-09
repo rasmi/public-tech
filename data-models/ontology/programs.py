b7bb2ffcfcd356c39a6b9b69791ef02830ce91d0
Rasmi Elasmar <rrelasmar@gmail.com>
2023-05-09 18:24:04 -0400

    Added generate_requirements_graph helper function.

--- data-models/ontology/programs.py
+++ data-models/ontology/programs.py
@@ -20,3 +20,27 @@ class ChildCare(Program):
     """Child Care Assistance program."""
 
     requirements = childcare.requirements
+
+
+def generate_requirements_graph(program):
+    """Generate a graph of the requirements for the program.
+
+    Returns:
+        nodes:
+            Nodes are information required by this program, and documents required
+                to prove each piece of information.
+        edges: Edges indicate which information requires which documents.
+    """
+    nodes = set()
+    edges = set()
+
+    nodes.add(program)
+
+    for info, docs in program.requirements.items():
+        nodes.add(info)
+        edges.add((program, info))
+        for doc in docs:
+            nodes.add(doc)
+            edges.add((info, doc))
+
+    return nodes, edges
