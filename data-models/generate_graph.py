ebf019695b17e3078ee7d4db7b455e87bae639a8
Rasmi Elasmar <rrelasmar@gmail.com>
2023-05-09 18:25:34 -0400

    Added helper script to generate JSON graph.

---
+++ data-models/generate_graph.py
@@ -0,0 +1,43 @@
+"""Helper script to generate a JSON graph of all program requirements."""
+import networkx as nx
+
+from ontology import programs
+
+
+def generate_programs_graph(programs_list):
+    """Generate a NetworkX graph of all program requirements.
+
+    TODO: Edges should actually be unique by program-info-document,
+        not just by program-info or info-document.
+    For example, some programs may accept certain documents as proof for some information,
+        but others may not. The current implementation obscures these differences by
+        merging all the info-document edges.
+    """
+
+    G = nx.Graph()
+    for program in programs_list:
+        program_nodes, program_edges = programs.generate_requirements_graph(program)
+        G.add_nodes_from(program_nodes)
+        G.add_edges_from(program_edges)
+
+    return G
+
+
+def graph_to_json(graph):
+    """Convert a NetworkX graph to json."""
+    nodes = [{"id": node.__name__} for node in graph.nodes()]
+    edges = [
+        {"source": edge[0].__name__, "target": edge[1].__name__}
+        for edge in graph.edges()
+    ]
+    return {"nodes": nodes, "edges": edges}
+
+
+if __name__ == "__main__":
+    import json
+
+    all_programs = [programs.SNAP, programs.ChildCare]
+    programs_graph = generate_programs_graph(all_programs)
+
+    with open("programs_graph.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(graph_to_json(programs_graph)))
