ab3febbfca68be2e58752b9f3c2723d10e715fe0
Rasmi Elasmar <rrelasmar@gmail.com>
2023-05-23 15:46:08 -0400

    Reformat with prettier.

--- process-map/src/App.tsx
+++ process-map/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ProcessMap from './process-map';
-import './App.css';
+import React from "react";
+import ProcessMap from "./process-map";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-        <ProcessMap />
+    <div className="App">
+      <ProcessMap />
     </div>
   );
 }
