b97f2f0365276a320c30b830216f2c4768a7af77
Rasmi Elasmar <rrelasmar@gmail.com>
2023-08-20 13:37:13 -0400

    Manually define templates.

--- public-assistant/service_guide.py
+++ public-assistant/service_guide.py
@@ -1,14 +1,12 @@
 """Service Guide that can identify relevant programs given basic info."""
-import pydantic
-from marvin.prompts.library import System
-
-from program_data import ProgramInfo
 
 
-class ServiceGuidePrompt(System):
+class ServiceGuidePrompt:
     """Prompt template for a Service Guide that can identify relevant programs given basic info."""
 
-    content: str = """
+    role: str = "system"
+
+    template: str = """
     You are a knowledgeable and compassionate public assistant helping struggling New York City residents understand what resources are available to them.
     The people coming to you are seeking critical government services and are not sure what support may be available or how to access it. 
     Use the information people share with you about their lives to identify which programs or services may be helpful to them. 
@@ -16,11 +14,31 @@ class ServiceGuidePrompt(System):
     Keep your guidance simple and succinct, as people may have difficulty understanding the complex details of these programs.
 
     Use the following database of information to direct people to the right programs, and only direct people to the programs listed below:
-
-    {% for program in programs %}
-        - {{program}}
-    {% endfor %}
+    {program_details}
     """
-    programs: list[ProgramInfo] = pydantic.Field(
-        ..., description="A list of programs to direct people to."
-    )
+    content: str
+
+    def __init__(self, program_details) -> None:
+        self.content = self.template.format(program_details=program_details)
+
+
+class ServiceRecommenderPrompt:
+    """Prompt template for a Service Guide that can identify relevant programs given basic info."""
+
+    role: str = "system"
+
+    template: str = """
+    You are a knowledgeable and compassionate public assistant helping struggling New York City residents understand what resources are available to them.
+    The people coming to you are seeking critical government services and are not sure what support may be available or how to access it.
+    Use the information people share with you about their lives to identify which programs or services may be helpful to them.
+    Prioritize programs that are especially relevant to the person's needs.
+
+    Respond only with a valid JSON list of 'program_name' strings that correspond to relevant programs.
+
+    Use the following database of information to direct people to the right programs, and only direct people to the programs listed below:
+    {program_details}
+    """
+    content: str
+
+    def __init__(self, program_details) -> None:
+        self.content = self.template.format(program_details=program_details)
